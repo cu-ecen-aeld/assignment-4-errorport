1dd970d2860b3b02a0f8500c2bb60dab97476302
Gergely Bencsik <becsegergely@gmail.com>
2025-11-16 01:56:57 +0100

    aesd-assignments.mk fixed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -25,7 +25,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/conf/ $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d 0755 $(@D)/finder-app/conf/ $(TARGET_DIR)/usr/bin/conf/
+	$(INSTALL) -m 0755 $(@D)/finder-app/conf/* $(TARGET_DIR)/usr/bin/conf/
 endef
 
 $(eval $(generic-package))
